b6fb434599c84cd122fc1524eea545b363b9d401
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2024-04-09 02:48:32 +0600

    buy validate

--- src/components/Products/BuyProduct.js
+++ src/components/Products/BuyProduct.js
@@ -1,13 +1,19 @@
 'use client'
 
+import useAuthChecker from "@/hooks/useAuthChecker";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import PaymentModal from "../UI/PaymentModal";
 
 export default function BuyProduct({ product }) {
     const [showModal, setShowModal] = useState(false)
+    const router = useRouter()
+    const auth = useAuthChecker()
+    if (auth === null) router.push('/signin')
+
     return (
         <>
-            {showModal && <PaymentModal onShowModal={setShowModal} product={product}/>}
+            {showModal && <PaymentModal onShowModal={setShowModal} product={product} />}
             <button onClick={() => setShowModal(true)} className="btn btn-primary focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">Get started</button>
         </>
     );
