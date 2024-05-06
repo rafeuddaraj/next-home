dfa481f33762135227863dca04b2afe436850757
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2024-05-06 19:35:32 +0600

    bug fixed

--- src/components/Dashboard/NextIrrigrator.js
+++ src/components/Dashboard/NextIrrigrator.js
@@ -61,8 +61,8 @@ export default function NextIrrigator() {
                     </div>
                     <div className={`mt-8 relative ${loading ? 'animate-pulse' : ''}`}>
 
-                        {responseStartStatus && <Image height={100} width={100} src="/tenor.gif" alt="Waterfall Status" className="w-full rounded-lg shadow-md h-32" />}
-                        {!responseStartStatus && trackFirst.current !== 1 && <video className="w-full rounded-lg shadow-md" autoPlay muted>
+                        {responseStartStatus && startStatus && <Image height={100} width={100} src="/tenor.gif" alt="Waterfall Status" className="w-full rounded-lg shadow-md h-32" />}
+                        {!responseStartStatus && startStatus && trackFirst.current !== 1 && <video className="w-full rounded-lg shadow-md" autoPlay muted>
                             <source src="/stop.mp4" type="video/mp4" />
                         </video>}
 
