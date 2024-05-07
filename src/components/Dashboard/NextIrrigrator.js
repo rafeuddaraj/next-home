358c6fd062d6de82e4923a3c4a5001a667c40423
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2024-05-07 19:21:08 +0600

    active status featured added

--- src/components/Dashboard/NextIrrigrator.js
+++ src/components/Dashboard/NextIrrigrator.js
@@ -9,6 +9,7 @@ export default function NextIrrigator() {
     const auth = useAuthChecker()
     const db = getDatabase(app)
     const trackFirst = useRef(1)
+    const localActive = useRef(null)
     const [irrigatorDetails, setIrrigatorDetails] = useState(null);
     useEffect(() => {
         if (auth) {
@@ -36,45 +37,58 @@ export default function NextIrrigator() {
         })
     }, [auth?.uid])
 
-    const { startStatus, waterLevel, responseStartStatus, loading } = irrigatorDetails || {}
+
+    const { startStatus, waterLevel, responseStartStatus, loading, active } = irrigatorDetails || {}
+    const activeCheck = () => {
+        if (active !== localActive.current) {
+            localActive.current = active
+            return true
+        }
+        return false
+    }
 
     // const [voltage, setVoltage] = useState(0)
     // const handleSlider = (e) => {
     //     setVoltage(e.target.value)
     // }
 
+    const activeStatus = activeCheck()
+
     return (
         <section className="my-10">
             <div className="!container">
                 <div className="w-full sm:w-[80%] md:w-[50%]  xl:w-[35%] mx-auto max-w-sm p-8 rounded-lg shadow-md bg-base-100">
+                    <h3>Active Status: {activeStatus ? "✅" : "❌"}</h3>
                     <h2 className="text-2xl font-semibold text-center mb-4">Water Control</h2>
                     <div className="flex justify-center items-center space-x-4">
-                        <button onClick={() => !startStatus && handleOnOFF(true)} className="px-4 py-2 rounded-md bg-green-500 text-white font-semibold hover:bg-green-700 focus:outline-none">Turn On</button>
+                        <button onClick={() => !startStatus && activeStatus && handleOnOFF(true)} className="px-4 py-2 rounded-md bg-green-500 text-white font-semibold hover:bg-green-700 focus:outline-none">Turn On</button>
                         <button className="px-4 py-2 rounded-md bg-gray-400 text-white font-semibold hover:bg-gray-600 focus:outline-none" onClick={() => {
-                            if (startStatus) {
+                            if (startStatus && activeStatus) {
                                 handleOnOFF(false)
                             }
                         }}>Turn Off</button>
                     </div>
-                    <div className="mt-8 relative">
+                    {
+                        activeStatus && <div className="mt-8 relative">
 
-                        {responseStartStatus && startStatus && !loading && <Image height={100} width={100} src="/tenor.gif" alt="Waterfall Status" className="w-full rounded-lg shadow-md h-32" />}
-                        {!responseStartStatus && !startStatus && !loading && trackFirst.current !== 1 && <video className="w-full rounded-lg shadow-md" autoPlay muted>
-                            <source src="/stop.mp4" type="video/mp4" />
-                        </video>}
-                        <div id="loadingOverlay" className="absolute inset-0 bg-gray-500 opacity-75 hidden justify-center items-center">
-                            <svg className="animate-spin h-10 w-10 text-white" viewBox="0 0 24 24">
-                            </svg>
-                            <span className="text-white ml-2">Loading...</span>
-                        </div>
-
-                        {loading && (
-                            <div className='p-10 text-blue-500 border shadow-lg animate-pulse'>
-                                Please Wait...
+                            {responseStartStatus && startStatus && !loading && <Image height={100} width={100} src="/tenor.gif" alt="Waterfall Status" className="w-full rounded-lg shadow-md h-32" />}
+                            {!responseStartStatus && !startStatus && !loading && trackFirst.current !== 1 && <video className="w-full rounded-lg shadow-md" autoPlay muted>
+                                <source src="/stop.mp4" type="video/mp4" />
+                            </video>}
+                            <div id="loadingOverlay" className="absolute inset-0 bg-gray-500 opacity-75 hidden justify-center items-center">
+                                <svg className="animate-spin h-10 w-10 text-white" viewBox="0 0 24 24">
+                                </svg>
+                                <span className="text-white ml-2">Loading...</span>
                             </div>
-                        )}
 
-                    </div>
+                            {loading && (
+                                <div className='p-10 text-blue-500 border shadow-lg animate-pulse'>
+                                    Please Wait...
+                                </div>
+                            )}
+
+                        </div>
+                    }
                 </div>
                 <svg id="refresh-icon" style={{ display: "none" }} viewBox="0 0 24 24">
                     <path fill="none" d="M12 5c-1.8 0-3.2 1.4-3.2 3s1.4 3 3.2 3 3.2-1.4 3.2-3-1.4-3-3.2-3zM7.16 16.84l4.84-4.84c.78-.78 2.05-.78 2.83 0l.78.78c.78.78.78 2.05 0 2.83l-4.84 4.84c-.78.78-2.05.78-2.83 0z" />
